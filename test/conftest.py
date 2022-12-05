a79f9a8f8f2619890045fa99756df80e13a697e5
alexandegrud <grudinkinalex0506@mai.ru>
2022-12-05 13:29:35 +0300

    Added new window sizes

--- test/conftest.py
+++ test/conftest.py
@@ -4,7 +4,8 @@ import pytest
 @pytest.fixture()
 def practice_form_open_browser():
     browser.config.hold_browser_open = True
-    browser.open('https://demoqa.com/automation-practice-form').driver.maximize_window()
+    browser.config.window_width = 1080
+    browser.config.window_height = 1920
 
     yield
     browser.close()
